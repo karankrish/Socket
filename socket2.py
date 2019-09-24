6d7b79b15ced336610151ab65f89a9e99bc65e70
Karan <mail2karankrishnan@gmail.com>
2019-09-24 16:35:46 +0530

    name change

--- socket2.py
+++ socket2.py
@@ -1,32 +1,21 @@
 from flask import Flask
-from flask_socketio import SocketIO, send , emit
-from flask_cors import CORS
-
+from flask_socketio import SocketIO , emit
 app = Flask(__name__)
 app.config["INFO"] = True
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
-
 def some_function(p):
-    print("===============")
     socketio.emit('testEmit', p)
-
 @socketio.on('test')
 def handleMessage(test):
     print(test)
-    
-    
 @socketio.on('connection')
 def handleMessage(asd):
     print(asd)
-
-
 @socketio.on('message')
 def handleMessage2(asd):
     print('Message: ' +asd)
     some_function(asd)
-
-    
 if __name__ == '__main__':
 	socketio.run(app,host="192.168.100.20",port=7333, certfile='cert.pem', keyfile='key.pem')
